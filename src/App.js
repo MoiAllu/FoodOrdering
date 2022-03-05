d4b673a6c23ce6c28940bab6b29e315dd53423b0
MoiAllu <al5690410@gmail.com>
2022-03-06 01:08:17 +0500

    further

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import {Fragment} from "react";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meal/Meals";
+import { Fragment } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meal/Meals';
 import Cart from './components/Cart/Cart';
+
 function App() {
   return (
     <Fragment>
-      <Cart/>
-      <Header></Header>
+      <Cart />
+      <Header />
       <main>
-        <Meals></Meals>
+        <Meals />
       </main>
     </Fragment>
   );
